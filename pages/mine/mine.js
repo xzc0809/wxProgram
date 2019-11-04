0011a196d06a044e2e8b95b2eb82ada3e52d7e28
肖智超 <421146350@qq.com>
2019-11-04 10:40:23 +0800

    我的页面的添加

---
+++ pages/mine/mine.js
@@ -0,0 +1,14 @@
+var videoUtil = require('../../utils/videoUtil.js')
+
+const app = getApp()
+
+Page({
+  data: {
+    faceUrl: "../resource/images/noneface.png",
+    
+  },
+  onLoad:function(){
+    
+  }
+
+})
