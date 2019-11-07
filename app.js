c87e422258c5bb08f00ed7d10852d404e4802693
肖智超 <421146350@qq.com>
2019-11-07 19:26:11 +0800

    修改了url

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //app.js
 App({
-  serverUrl: "https://fc1e9a8a.ngrok.io",
+  serverUrl: "https://b94f1839.ngrok.io",
   userInfo: null,
   // userfileUrl:'H:\gitReponsitory\douyin\douyin_userFiles\191103F961KC7GHH\face',//已经映射到tomcat下
   globalData: {
