9e025b5e5e4f6cc8c8e7214b5d96cd31774c649b
肖智超 <421146350@qq.com>
2019-11-08 21:22:07 +0800

    首页 视频截图展示业务逻辑
    上拉刷新，下滑展示下一页，单击事件

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,93 @@
-//index.js
-//获取应用实例
 const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    screenWidth: 350,
+    serverUrl:app.serverUrl,
+    pageNo:1,
+    pageCount:null,
+    videoList:[],
+    postUrl:null
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+
+  onLoad: function (params) {
+    var me = this;
+    var serverUrl=me.data.serverUrl;
+    var screenWidth = wx.getSystemInfoSync().screenWidth;//获取当前手机屏幕信息的api
+    me.setData({
+      screenWidth: screenWidth,
+    });
+    wx.showLoading({
+      title: '获取中...',
+    });
+    me.showVideo(me.data.pageNo)
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
+
+  showVideo:function(pageNo){//显示视频
+    var me=this;
+    var postUrl = me.data.serverUrl + '/video/showAll?pageNo=' + pageNo;
+    console.log("触发");
+    console.log("触发showVideo"+postUrl)
+    wx.request({
+      url: postUrl,
+      method: 'POST',
+      dataType: 'json',
+      success: function (res) {
+
+        var data = res.data;
+        console.log(res.data);
+        if (data.status == 200) {
+          wx.hideLoading();
+          if(pageNo===1){//当为第一页时，把累加list的清空
+            me.setData({
+                videoList:[]
+            })
+          }
+
+          var newVideoList=me.data.videoList.concat(data.data.rows);
+          me.setData({
+            videoList: newVideoList,
+            pageCount: data.data.pageCount,
+            pageNo:pageNo
+          })
+          wx.hideNavigationBarLoading();//隐藏标题刷新图标
+          wx.stopPullDownRefresh();//停止下拉刷新
+          console.log(me.data.pageCount)
+
+        } else {
+          wx.showToast({
+            title: '获取失败，请重试',
+            icon: 'none'
           })
         }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+
+      },
+
     })
+  },
+
+  onReachBottom:function(){
+
+    var me=this;
+    var pageNo=me.data.pageNo;
+    //判断是否最后一页
+    if(pageNo===me.data.pageCount){
+      wx.showToast({
+        title: '到底了',
+        icon: 'none'        
+      });
+      return;
+    }
+    pageNo=pageNo+1;
+    me.showVideo(pageNo);
+
+  },
+  onPullDownRefresh:function(){//下拉刷新
+  wx.showNavigationBarLoading();//标题刷新圆圈
+    this.showVideo(1);
+  },
+  showVideoInfo:function(e){
+    console.log(e.detail.value);
   }
+   
 })
