e2d4ada9d30216ebfc37c7e0c9bc42a822e8816f
肖智超 <421146350@qq.com>
2019-11-11 16:28:47 +0800

    设置全局userInfo的方式修改

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 //app.js
 App({
-  serverUrl: "https://b94f1839.ngrok.io",
+  serverUrl: "http://127.0.0.1:8085",
+  // serverUrl:"http://192.168.137.1:8085",
+  // serverUrl: "https://d1e97379.ngrok.io",
   userInfo: null,
   // userfileUrl:'H:\gitReponsitory\douyin\douyin_userFiles\191103F961KC7GHH\face',//已经映射到tomcat下
   globalData: {
     serverUrl: "http://c4982807.ngrok.io:8085",
     userInfo: null,
     userfileUrl: "s"
+  },
+  setGlobalUserInfo:function(user){
+    wx.setStorageSync("userInfo", user)
+  },
+  getGlobalUserInfo:function(){
+    return wx.getStorageSync("userInfo");
   }
 })
